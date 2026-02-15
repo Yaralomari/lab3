8dec94ba4d581a5bc9673864e225d32b8133b20a
Yara <yaro0o0111@gmail.com>
2026-02-15 22:41:42 +0300

    Lab 3 Django

---
+++ apps/bookmodule/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    name = request.GET.get("name") or "world"
+    return render(request, "bookmodule/index.html", {"name": name})
+
+def index2(request, val1=0):
+    return HttpResponse("value1 = " + str(val1))
